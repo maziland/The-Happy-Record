884606d278cb018b18c3dc78a87ef9c066303369
TheMaziland <alon951m@gmail.com>
2023-05-31 18:53:07 +0300

    Added a fetcher to get all TOP 200 ALBUMS of all time, in order to create a DB

---
+++ utils/top_200_albums_fetch.js
@@ -0,0 +1,53 @@
+const axios = require('axios').default;
+const JSSoup = require('jssoup').default;
+const he = require('he');
+
+var albums = []
+
+function nameTrim(str) {
+    return he.decode(str.replace("\r\n", "").replace(/\s\s+/g, ' '));
+}
+
+async function getRequest(url) {
+    try {
+        const response = (await axios.get(url));
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getAlbumsFromPage(url) {
+    console.log(url);
+    htmlData = await getRequest(url);
+    const soup = new JSSoup(htmlData);
+    table = soup.find("table");
+    table_rows = table.findAll("td", "main_entry").forEach(entry => {
+        row = entry.parent;
+        album = {
+            "number": row.find("td", "number").text,
+            "coverImage": row.find("td", "list_art").find("img").attrs["data-src"],
+            "singerName": nameTrim(row.find("td", "main_entry").find("h2").find("a").text),
+            "albumName": nameTrim(row.find("td", "main_entry").find("h3").find("a").text),
+            "releaseYear": row.find("td", "main_entry").find("h3").find("span").text.replace("(", "").replace(")", "")
+        }
+        albums = albums.concat(album);
+    });
+};
+
+async function getAlbumsFromAllPages() {
+    // baseUrl = "https://rateyourmusic.com/list/CrabbyLoafers/billboard-greatest-of-all-time-billboard-200-albums/"
+    baseUrl = "http://127.0.0.1:8000/"
+    index = 1;
+    while (index <= 8) {
+        url = baseUrl + index + ".html";
+        console.log(`Scraping index ${index}`);
+        console.log(url);
+        await getAlbumsFromPage(url);
+        index++;
+    }
+    console.log(albums);
+    console.log("finished");
+};
+
+getAlbumsFromAllPages();
