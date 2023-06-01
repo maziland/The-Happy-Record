c6b7c94fb52e7427d81a15e020b528a1777f2d25
TheMaziland <alon951m@gmail.com>
2023-06-01 08:53:23 +0300

    Added albums page, with fetced DB data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path")
 const fs = require("fs")
 const mongoose = require("mongoose")
 require("dotenv").config()
+const ejs = require('ejs');
 
 const albumService = require("./services/album")
 const userService = require("./services/user")
@@ -22,9 +23,15 @@ userService.uploadJson(JSON.parse('{"_id":"5","_name":"user"}'), override = true
 
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get('/', async (req, res) => {
+    console.log("aaa");
+    const model = require("./models/album")
+    const albumModel = model.albumModel;
+    const collection = albumModel.collection;
+    const albums = await collection.find({}).toArray();
+    const renderedHtml = await ejs.renderFile('public/index.ejs', { albums });
+    res.send(renderedHtml);
 });
 
-app.listen(process.env.LISTE_PORT || 3000)
+app.listen(process.env.LISTEN_PORT || 3000)
 console.log(`Running at Port ${process.env.LISTEN_PORT || 3000}`);
