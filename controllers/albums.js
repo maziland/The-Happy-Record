efa3431be7a398761c37b33c434f354a8abe7347
TheMaziland <alon951m@gmail.com>
2023-06-23 20:25:21 +0300

    Comments

--- controllers/albums.js
+++ controllers/albums.js
@@ -10,14 +10,17 @@ async function homepage(req, res) {
     res.render("homepage.ejs");
 };
 
+
+// This function returns the albums.ejs only with albums
+// found by the search query.
+// Searching based on album name or artist name
 async function search(req, res) {
     const searchString = req.query.q;
     const collection = albumModel.collection;
     const albums = await collection.find({
         $or: [
             { name: { $regex: searchString, $options: 'i' } },
-            { artist: { $regex: searchString, $options: 'i' } },
-            { releaseYear: { $regex: searchString, $options: 'i' } },
+            { artist: { $regex: searchString, $options: 'i' } }
         ]
     }).toArray();
 
