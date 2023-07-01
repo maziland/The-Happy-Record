f75549317653b098beb9b6ec1c71cf82d761d183
TheMaziland <alon951m@gmail.com>
2023-07-01 14:25:35 +0300

    A working modal for the shopping cart on cart button click

---
+++ public/js/cart.js
@@ -0,0 +1,7 @@
+// All functions related to shopping cart management goes here
+$(document).ready(function () {
+    $("#shopping-cart").click(() => {
+        // On click, open the cart modal
+        $('#exampleModal').modal('show');
+    });
+});
