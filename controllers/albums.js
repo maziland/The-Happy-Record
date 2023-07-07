423d749b0bd80618240a04bc006a3519b27ce219
TheMaziland <alon951m@gmail.com>
2023-07-07 18:15:00 +0300

    Added pagination and sorting.

--- controllers/albums.js
+++ controllers/albums.js
@@ -2,8 +2,27 @@ const albumModel = require("../models/album").albumModel
 
 async function mainAlbums(req, res) {
     const collection = albumModel.collection;
-    const albums = await collection.find({}).toArray();
-    res.render('albums.ejs', { albums });
+    const allAlbums = await collection.find({}).toArray();
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    const sort = req.query.sort || 'default'; // Default sorting option if not provided
+
+    // Define the number of albums to display per page
+    const albumsPerPage = 20;
+
+    // Calculate the start and end indices of the albums to display
+    const startIndex = (page - 1) * albumsPerPage;
+    const endIndex = page * albumsPerPage;
+
+    // Slice the albums array based on the calculated indices
+    const displayedAlbums = allAlbums.slice(startIndex, endIndex);
+
+    logger.debug(`Rendering albums with sorting: ${sort}, albums per page: ${albumsPerPage}`);
+    res.render('albums.ejs', {
+        albums: displayedAlbums,
+        currentPage: page,
+        totalPages: Math.ceil(allAlbums.length / albumsPerPage),
+        sortOption: sort
+    });
 };
 
 async function homepage(req, res) {
