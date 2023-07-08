fe704219900f408b090bcc12c06b588f9879e1bc
TheMaziland <alon951m@gmail.com>
2023-07-08 20:31:00 +0300

    Added album sorting, also between pages

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,11 +1,24 @@
 const albumModel = require("../models/album").albumModel
-
+function sortArrayByParameter(array, parameter) {
+    switch (parameter) {
+        case 'rank':
+            return array.sort((a, b) => a.rank - b.rank);
+        case 'title':
+            return array.sort((a, b) => a.name.localeCompare(b.name));
+        case 'artist':
+            return array.sort((a, b) => a.artist.localeCompare(b.artist));
+        case 'price_lth':
+            return array.sort((a, b) => a.price - b.price);
+        case 'price_htl':
+            return array.sort((a, b) => b.price - a.price);
+        default:
+            return array;
+    }
+}
 async function mainAlbums(req, res) {
     const collection = albumModel.collection;
-    const allAlbums = await collection.find({}).toArray();
     const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
-    const sort = req.query.sort || 'default'; // Default sorting option if not provided
-
+    const sort = req.query.sort || 'rank'; // Default sorting option if not provided
     // Define the number of albums to display per page
     const albumsPerPage = 20;
 
@@ -13,14 +26,18 @@ async function mainAlbums(req, res) {
     const startIndex = (page - 1) * albumsPerPage;
     const endIndex = page * albumsPerPage;
 
+    // Sort albums
+    var allAlbums = await collection.find({}).toArray();
+    sortedAlbums = sortArrayByParameter(allAlbums, sort);
+
     // Slice the albums array based on the calculated indices
-    const displayedAlbums = allAlbums.slice(startIndex, endIndex);
+    const displayedAlbums = sortedAlbums.slice(startIndex, endIndex);
 
     logger.debug(`Rendering albums with sorting: ${sort}, albums per page: ${albumsPerPage}`);
     res.render('albums.ejs', {
         albums: displayedAlbums,
         currentPage: page,
-        totalPages: Math.ceil(allAlbums.length / albumsPerPage),
+        totalPages: Math.ceil(sortedAlbums.length / albumsPerPage),
         sortOption: sort
     });
 };
@@ -36,14 +53,19 @@ async function homepage(req, res) {
 async function search(req, res) {
     const searchString = req.query.q;
     const collection = albumModel.collection;
-    const albums = await collection.find({
+    const displayedAlbums = await collection.find({
         $or: [
             { name: { $regex: searchString, $options: 'i' } },
             { artist: { $regex: searchString, $options: 'i' } }
         ]
     }).toArray();
 
-    res.render('albums.ejs', { albums });
+    res.render('albums.ejs', {
+        albums: displayedAlbums,
+        currentPage: 1,
+        totalPages: 0,
+        sortOption: "rank"
+    });
 };
 
 module.exports = { mainAlbums, homepage, search };
